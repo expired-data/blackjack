3432a59bf2be19104bcc0288eb4da528e5b6d0b1
A Ridley <expireddata@gmail.com>
2021-03-26 12:05:29 +0000

    make ip configurable

--- apps/blackjack_server/lib/blackjack_server/application.ex
+++ apps/blackjack_server/lib/blackjack_server/application.ex
@@ -9,11 +9,12 @@ defmodule BlackjackServer.Application do
   def start(_type, _args) do
     tcpPort = String.to_integer(System.get_env("TCP_PORT") || "4040")
     httpPort = String.to_integer(System.get_env("HTTP_PORT") || "4041")
+    ip = List.to_tuple(for i <- String.split(System.get_env("HOST_IP") || "127.0.0.1", "."), do: elem(Integer.parse(i),0))
 
     children = [
       {Task.Supervisor, name: BlackjackServer.TaskSupervisor},
       Supervisor.child_spec({Task, fn -> BlackjackServer.accept(tcpPort) end}, restart: :permanent),
-      {Plug.Cowboy, scheme: :http, plug: BlackjackServer.Plug, options: [port: httpPort, ip: {192,168,1,40}]},
+      {Plug.Cowboy, scheme: :http, plug: BlackjackServer.Plug, options: [port: httpPort, ip: ip]},
       BlackjackServer.PIDAgent
     ]
 
